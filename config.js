fb92e4e739cb1a1916c54d940842b44c98bf985b
Prince mayel <145528426+Princemaye@users.noreply.github.com>
2025-12-02 00:36:04 +0100

    Format config.js for better readability

--- config.js
+++ config.js
@@ -1,8 +1,10 @@
-require('dotenv').config(); 
-module.exports = { port: process.env.PORT || '3000', 
-emailPort: process.env.EMAIL_PORT || '465',
-emailHost: process.env.EMAIL_HOST || 'smtp.zoho.com', 
-emailUser: process.env.EMAIL_USER || 'support@princetechn.com', 
-emailPass: process.env.EMAIL_PASS || 'Princemayel123@', 
-emailFrom: process.env.EMAIL_FROM || '"Princetechn Support" support@princetechn.com', 
-emailReplyTo: process.env.EMAIL_REPLY_TO || 'support@princetechn.com', };
+require('dotenv').config();
+module.exports = {
+    port: process.env.PORT || '3000',
+    emailPort: process.env.EMAIL_PORT || '465',
+    emailHost: process.env.EMAIL_HOST || 'smtp.zoho.com',
+    emailUser: process.env.EMAIL_USER || 'support@princetechn.com',
+    emailPass: process.env.EMAIL_PASS || 'Princemayel123@',
+    emailFrom: process.env.EMAIL_FROM || '"Princetechn Support" <support@princetechn.com>',
+    emailReplyTo: process.env.EMAIL_REPLY_TO || 'support@princetechn.com',
+};
