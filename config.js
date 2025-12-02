095439553c0c5ee4b13ccbc89bccbef866b4d7ee
Prince mayel <145528426+Princemaye@users.noreply.github.com>
2025-12-02 18:06:00 +0100

    Refactor email configuration in config.js
    Updated email configuration settings and added new properties.

--- config.js
+++ config.js
@@ -1,7 +1,18 @@
+
 module.exports = {
-    port: process.env.PORT || 3000,
-    emailUser: process.env.EMAIL_USER,
-    clientId: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-    refreshToken: process.env.ZOHO_REFRESH_TOKEN
+
+port: process.env.PORT || '3000',
+
+emailPort: process.env.EMAIL_PORT || '465',
+
+emailHost: process.env.EMAIL_HOST || 'smtp.zoho.com',
+
+emailUser: process.env.EMAIL_USER || 'support@princetechn.com',
+
+emailPass: process.env.EMAIL_PASS || 'Princemayel123@',
+
+emailFrom: process.env.EMAIL_FROM || '"Princetechn Support" <support@princetechn.com>',
+
+emailReplyTo: process.env.EMAIL_REPLY_TO || 'support@princetechn.com',
+
 };
